57dd8d1d6eac7ecb1c75e64ffb0bc76a4d63dd70
rareresolutions <41582762+rareresolutions@users.noreply.github.com>
2022-11-16 21:36:33 +0100

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ Then go to sleep on heaps of treasure. They’ve no use for the written word.</l
                 $q5 = $_POST['question5'];
            //}
                 
-                     $file = fopen("./api/comment.html","a");
+                     $file = fopen("guestbook","a");
                      fwrite($file, "<div class='gb'>
                      Dear&nbsp;".$fname."<br>
                      Most dragons don’t know how to read. They hiss and fume and guard their hoard. A tasty knight is what they need&nbsp;"
@@ -53,7 +53,7 @@ Then go to sleep on heaps of treasure. They’ve no use for the written word.</l
                      fclose($file);
                     }
             
-               require("./api/comment.html");
+               require("guestbook");
 ?>
 </div>
 
