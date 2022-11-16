99cd09583660d66d24d70b449eb31b8d585083b3
rareresolutions <41582762+rareresolutions@users.noreply.github.com>
2022-11-16 21:30:58 +0100

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Basic GuestBook By the Wheelchair Guy</title>
-    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="./css/main.css">
     <script>
      if ( window.history.replaceState ) {
   window.history.replaceState( null, null, window.location.href );
